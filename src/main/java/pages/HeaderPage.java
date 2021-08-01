6c0a257a2976f6dd7a28694b40186431fc9bd091
Liza Kukharenko <elizaveta.kuharenko@gmail.com>
2021-08-01 19:02:39 +0300

    changes in HeaderPage and TopStickyMenu

--- src/main/java/pages/HeaderPage.java
+++ src/main/java/pages/HeaderPage.java
@@ -5,4 +5,24 @@ import org.openqa.selenium.WebDriver;
 public class HeaderPage extends BasePage{
 
     public HeaderPage(WebDriver driver){super(driver);}
+
+    public void clickHomePageLogo(){}
+
+    public void clickSalePage(){}
+
+    public void clickNewPage(){}
+
+    public void clickWomenPage(){}
+
+    public void clickMenPage(){}
+
+    public void clickKidsPage(){}
+
+    public void clickPlusSizePage(){}
+
+    public void clickSearchLink(){}
+
+    public void clickSignInLink(){}
+
+    public void clickShoppingBagLink(){}
 }
--- src/main/java/pages/TopStickyMenu.java
+++ src/main/java/pages/TopStickyMenu.java
@@ -1,2 +1,14 @@
-package pages;public class TopStickyMenu {
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class TopStickyMenu extends BasePage{
+
+    public TopStickyMenu(WebDriver driver){super(driver);}
+
+    public void hoverClothingMenuItem(){}
+
+    public void clickFilterAndOrderMenuItem(){}
+
+    public void clickSeeCount(){}
 }
